adfd7e32bca6e26dda5483a1d63fce531dff896e
shefali02qq <shefalisingh870@gmail.com>
2025-07-06 16:55:00 +0530

    changed url

--- jobportal-yt-main/backend/index.js
+++ jobportal-yt-main/backend/index.js
@@ -17,8 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: [
+        'http://localhost:5173',
+        'https://jobportal-yt-main-frontend-q5t7.onrender.com'
+    ],
+    credentials: true
 }
 
 app.use(cors(corsOptions));
